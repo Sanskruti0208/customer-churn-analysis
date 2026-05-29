8f1d98ba9d0240a285db74823d8e6a5b3b599592
Sanskruti Tidke <130908126+Sanskruti0208@users.noreply.github.com>
2026-05-29 21:44:40 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,16 +1,7 @@
 import pandas as pd
 
-
 def load_dataset():
 
-    df = pd.read_csv("C:/Users/sansk/Downloads/customer-churn-analysis/data/customer_churn.csv")
-
-    print(df.head())
-
-    print("\nDataset Shape:")
-    print(df.shape)
-
-    print("\nColumns:")
-    print(df.columns)
+    df = pd.read_csv("data/customer_churn.csv")
 
     return df
